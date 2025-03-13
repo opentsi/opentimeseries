2ff7c6cad6960540c39f3914283bc746cc4f5632
Minna Heim <91681064+minnaheim@users.noreply.github.com>
2025-03-13 18:04:49 +0100

    added hours to get_commit_by_date

--- R/helpers.R
+++ R/helpers.R
@@ -1,4 +1,3 @@
-
 #' Get a List of All Tags from a Remote Time Series Archive
 #'
 #' For now, opentimeseries only supports GitHub as an arvchive host.
@@ -6,9 +5,11 @@
 #'
 #' @importFrom jsonlite fromJSON
 #' @export
-tags_list <- function(remote_archive){
-  api_url <- sprintf("https://api.github.com/repos/%s/tags",
-  remote_archive)
+tags_list <- function(remote_archive) {
+  api_url <- sprintf(
+    "https://api.github.com/repos/%s/tags",
+    remote_archive
+  )
   tags <- fromJSON(api_url, simplifyVector = FALSE)
   sapply(tags, "[[", "name")
 }
@@ -20,38 +21,48 @@ tags_list <- function(remote_archive){
 #' @importFrom httr2 request req_headers req_perform resp_body_json
 #' @export
 get_commit_dates <- function(remote_archive = "opentsi/kofethz",
-                             lastn = 5){
-  url <- sprintf("https://api.github.com/repos/%s/commits?sha=main&per_page=%d",
-                 remote_archive, lastn)
+                             lastn = 5) {
+  url <- sprintf(
+    "https://api.github.com/repos/%s/commits?sha=main&per_page=%d",
+    remote_archive, lastn
+  )
   res <- request(url) |>
     req_headers(Accept = "application/vnd.github.v3+json") |>
     req_perform() |>
     resp_body_json()
 
-  rbindlist(lapply(res, function(x) list(hash = x$sha,
-                               date = as.POSIXct(x$commit$author$date,
-                                                 format = "%Y-%m-%dT%H:%M:%SZ",
-                                                 tz = "UTC")
-                               )))
+  rbindlist(lapply(res, function(x) {
+    list(
+      hash = x$sha,
+      date = as.POSIXct(x$commit$author$date,
+        format = "%Y-%m-%dT%H:%M:%SZ",
+        tz = "UTC"
+      )
+    )
+  }))
 }
 
-
-get_commit_by_date <- function(dd, d){
-  filtered <- dd[as.POSIXct(d) > dd$date,]
-  index_of_max <- which.max(as.numeric(dd[as.POSIXct(d) > dd$date,]$date))
-  filtered[index_of_max,]$hash
+# dd = commit dates, d = date
+get_commit_by_date <- function(dd, d) {
+  # if d only yyyy-mm-dd not hh:mm:ss -> convert d to 23:59:59
+  if (!grepl("\\d{2}:\\d{2}:\\d{2}$", as.POSIXct(d))) {
+    d <- paste(d, "23:59:59")
+  }
+  filtered <- dd[as.POSIXct(d) > dd$date, ]
+  index_of_max <- which.max(as.numeric(filtered$date))
+  filtered[index_of_max, ]$hash
 }
-
 
 #' Find the Past Version That Is Closest to a Given Date
 #'
 #' @export
-find_version <- function(ref_date, tags,
-   return_as_tag = FALSE){
-  d <- as.Date(gsub("v","", tags))
+find_version <- function(
+    ref_date, tags,
+    return_as_tag = FALSE) {
+  d <- as.Date(gsub("v", "", tags))
   out <- max(d[ref_date > d])
-  if(return_as_tag){
-    out <- sprintf("v%s",out)
+  if (return_as_tag) {
+    out <- sprintf("v%s", out)
   }
   out
 }
@@ -62,18 +73,20 @@ find_version <- function(ref_date, tags,
 #'
 #' @importFrom data.table dcast
 #' @export
-triangle <- function(dt){
+triangle <- function(dt) {
   d <- dcast(dt, formula = date ~ id, value.var = "value")
-  names(d) <- gsub("(.+)(v[0-9]{4})","\\2",names(d))
+  names(d) <- gsub("(.+)(v[0-9]{4})", "\\2", names(d))
   d
 }
 
 # https://raw.githubusercontent.com/opentsi/kofethz/4d10e6332b2c37c4427df444be960f4d7e2cdec7/ch/kof/globalbaro/coincident/series.csv
-generate_gh_url <- function(series_path,
-   base_url = "https://raw.githubusercontent.com/",
-   remote_archive,
-   sha){
-  full_url <- sprintf("%s%s/%s/%s/series.csv",
+generate_gh_url <- function(
+    series_path,
+    base_url = "https://raw.githubusercontent.com/",
+    remote_archive,
+    sha) {
+  full_url <- sprintf(
+    "%s%s/%s/%s/series.csv",
     base_url,
     remote_archive,
     sha,
@@ -86,17 +99,11 @@ generate_gh_url <- function(series_path,
 key_to_path <- function(key, root_folder = "../ts_archive", remote = FALSE) {
   l <- strsplit(key, "\\.")
   sapply(l, function(x) {
-      if (remote) {
-          o <- do.call(file.path, as.list(x))
-          file.path(o, "series.csv")
-      } else {
-          do.call(file.path, as.list(x))
-      }
+    if (remote) {
+      o <- do.call(file.path, as.list(x))
+      file.path(o, "series.csv")
+    } else {
+      do.call(file.path, as.list(x))
+    }
   })
 }
-
-
-
-
-
-
