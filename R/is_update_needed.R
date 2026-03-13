70b86171fb1979ee714e28168a640ddcb5b1de58
Matt Bannert <bunny@hey.com>
2026-03-13 15:27:46 +0100

    update checksum process

---
+++ R/is_update_needed.R
@@ -0,0 +1,27 @@
+
+#' @importFrom digest digest
+#' @export
+is_update_needed <- function(request_checksum){
+  checksum_equal <- compare_checksums(
+    rq_checksum = request_checksum,
+    mi_checksum = mi$update_checksum
+    )
+  return(!checksum_equal)
+}
+
+
+#' @importFrom jsonlite function
+compare_checksums <- function(rq_checksum, mi_checksum){
+  mi <- fromJSON("inst/metadata.json")
+  mi$update_checksum == rq_checksum
+}
+
+
+#' @importFrom jsonlite fromJSON
+#' @export
+update_checksum <- function(cs, json_path="inst/metadata.json"){
+  mi <- fromJSON("")
+  mi$update_checksum <- cs
+  json_content <- toJSON(mi, pretty = TRUE, auto_unbox = TRUE)
+  writeLines(json_content, json_path)
+}
